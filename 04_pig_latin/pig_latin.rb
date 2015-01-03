abdea48c83d8dba2229415fc68edd0474ca1fff6
StevenXL <leiva.steven@gmail.com>
2015-01-03 15:02:41 -0500

    Finished pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,5 +1,27 @@
+# i have a hard time coding with the splat operator (used in the translate
+# method below). Let's review how the splat operator works:
+# 
+# The splat operator is used to create methods that have a variable parameter
+# list - i.e., they can take any number of parameters. This parameter list is,
+# in fact, a list, and what the splat operator does is it converts that list
+# into an array. (This is according to Ruby Monk - I was under the impression
+# that, in Ruby, a list is an array). 
+#
+# After conversion, the array is referenced without the splat operator. But the
+# key, again, is that it converts the parameters into an array!
+
 def translate(string)
-    return translate_single_word(string)
+    output = Array.new 
+
+    # turn our string into an array
+    words = string.split(' ')
+
+    # run each element in the array through our single-world translator method
+    words.each do |single_word|
+        output << translate_single_word(single_word)
+    end
+
+    return output.join(" ")
 end
 
 def translate_single_word(english)
@@ -9,8 +31,13 @@ def translate_single_word(english)
     # append to convert from english to pig latin 
     pig_appendage = "ay" 
     
-    # where does the first vowel occur
-    first_vowel = english.index(/[aeiou]/)
+    # does the word contain 'qu
+    if english.slice(0, 3).index("qu")
+        english[0] == "q" ? first_vowel = 2 : first_vowel = 3
+    else
+        # where does the first vowel occur
+        first_vowel = english.index(/[aeiou]/)
+    end
 
     # take whatever is before the first vowel, and append it to the word
     # then append pig_appendage to make it into pig latin
