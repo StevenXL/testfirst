b1271723e01166cbce346ea8469ab91c40fc0c93
StevenXL <leiva.steven@gmail.com>
2015-01-04 17:31:52 -0500

    working on xml document lesson

---
+++ 13_xml_document/xml_document.rb
@@ -0,0 +1,10 @@
+class XmlDocument
+    def hello(options = {})
+        if options.empty?
+            '<hello/>'
+        else
+            options.each do | attribute, value |
+            end
+        end
+    end
+end
