2025e267dcd9e301ef3dc83cfd52abf41b1e07c2
Steven Leiva <leiva.steven@gmail.com>
2015-10-17 13:15:26 -0400

    Add test for the multiply method.

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,9 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    multiply([7, 11]).should == 77
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    multiply([7, 11, 10]).should == 770
+  end
 
 end
 
